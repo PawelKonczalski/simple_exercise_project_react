b7355502f658b6989dbee9f8f3ff135ff8a65ccb
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-17 16:51:15 +0200

    list item v.2

---
+++ 10-ListItems-v2/app.js
@@ -0,0 +1,21 @@
+const Item = (props) => <li>{`Fruit ${props.content}`}</li>;
+
+class ListItems extends React.Component {
+    state = {
+        items: ['apple', 'plum', 'pear']
+    };
+
+    render() {
+        const Items = this.state.items.map(item=> <Item key={item} content={item}/>);
+
+        return (
+            <>
+                <ul>
+                    {Items}
+                </ul>
+            </>
+        )
+    }
+}
+
+ReactDOM.render(<ListItems/>, document.getElementById('root'));
