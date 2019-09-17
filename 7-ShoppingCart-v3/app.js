0aff0a40bcef75c74989ae35ada981da2423067e
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-09-17 16:42:38 +0200

    shopping cart v.3

---
+++ 7-ShoppingCart-v3/app.js
@@ -0,0 +1,47 @@
+class App extends React.Component {
+    state = {
+        availableProducts: 7,
+        shoppingCart: 0
+    };
+
+    handleRemoveFromCart = () => {
+        this.setState({
+            shoppingCart: this.state.shoppingCart - 1
+        })
+    };
+
+    handleAddToCart = () => {
+        this.setState({
+            shoppingCart: this.state.shoppingCart + 1
+        })
+    };
+
+    handleBuy = () => {
+        this.setState({
+            availableProducts: this.state.availableProducts - this.state.shoppingCart,
+            shoppingCart: 0
+        })
+    };
+
+    render() {
+        const style = this.state.shoppingCart === 0 ? {opacity: .3} : {};
+
+        return (
+            <>
+                <button
+                    disabled={this.state.shoppingCart === 0}
+                    onClick={this.handleRemoveFromCart}>-
+                </button>
+                <span style={style}> {this.state.shoppingCart} </span>
+                <button
+                    disabled={this.state.shoppingCart === this.state.availableProducts}
+                    onClick={this.handleAddToCart}>+
+                </button>
+                {this.state.shoppingCart > 0 && <button onClick={this.handleBuy}>Buy</button>}
+            </>
+        )
+    }
+
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
